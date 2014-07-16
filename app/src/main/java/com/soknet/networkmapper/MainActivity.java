52e9a5ce59ca6ebbe1c6c1bc947b9b551df89e31
Soknet <soknet@users.noreply.github.com>
2014-07-16 13:48:03 +0200

    init

--- app/src/main/java/com/soknet/networkmapper/MainActivity.java
+++ app/src/main/java/com/soknet/networkmapper/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toast tostada;
+        Toast tostaRica;
     }
 
 
